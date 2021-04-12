18703fc2370bc6be1cce0d381508858125e51c26
SeanSnake93 <SeanSnake93@hotmail.co.uk>
2021-04-12 11:01:32 +0100

    Update

--- Terraform/builds/manager/outputs.tf
+++ Terraform/builds/manager/outputs.tf
@@ -1,4 +1,3 @@
 output "system_public-ip" {
-  value = "http://${module.ec2_manager.system_public-ip[0]}"
-  
+  value = "http://${module.ec2_manager.system_public-ip[0]}\nssh -i \"~/.ssh/AccessKey\" ubuntu@ec2-${module.ec2_manager.system_public-ip[0]}.eu-west-1.compute.amazonaws.com\nls ~ Once ao appears\naws configure ~ Enter your details one last time"
 }
