4e66e5a402f799ad35f754659264456489bfb1c0
SeanSnake93 <SeanSnake93@hotmail.co.uk>
2021-04-12 11:39:26 +0100

    Quick mod

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 locking="false"
 temp_location="eu-west-1"
+Token=""
+IPLink=""
 cd Terraform/builds/worker/
 echo ""
 echo "        :::::::::  ::::::::: :::::::::: :::::::::: :::::::::   ::::::::  :::   :::  "
@@ -11,7 +13,7 @@ echo "    +#+    +#+ +#+              +#+    +#+     +#+    +#+ +#+    +#+    +#
 echo "   #+#    #+# #+#       #+#    #+#    #+#     #+#    #+# #+#    #+#    #+#          "
 echo "  #########  ######### #########     ###     ###    ###  ########     ###           "
 echo ""
-terraform destroy -var locked="${locking}" -var aws_location="${temp_location}" -auto-approve
+terraform destroy -var locked="${locking}" -var IPLink="${IPLink}" -var Token="${Token}" -var aws_location="${temp_location}" -auto-approve
 echo ""
 cd ./../manager/
 terraform destroy -var locked="${locking}" -var aws_location="${temp_location}" -auto-approve
